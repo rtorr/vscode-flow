7afd894a44a860975370733344f039b46e9d0e63
Richard Torruellas <richard.torruellas@am.sony.com>
2016-08-14 10:08:48 -0700

    license update

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,3 @@
-/*
- Copyright (c) 2015-present, Facebook, Inc.
- All rights reserved.
- *
- * This source code is licensed under the license found in the LICENSE file in
- the root directory of this source tree.
- */
-
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
